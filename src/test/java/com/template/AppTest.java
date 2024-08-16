a07f111193335d8776877e139327fede6b1fa20d
Facugarciar0805 <162722335+Facugarciar0805@users.noreply.github.com>
2024-08-16 12:36:24 -0300

    Update AppTest.java

--- src/test/java/com/template/AppTest.java
+++ src/test/java/com/template/AppTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static com.template.App.deposit;
+import static com.template.App.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
@@ -13,6 +13,75 @@ class AppTest {
     void testDeposit() {
         Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
         Map<String, Integer> updatedAccounts = deposit(accounts, "mati", 5);
-        assertEquals(true, true);
+        assertEquals(15, updatedAccounts.get("mati"));
     }
+    @Test
+    void testUnknownDeposit(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = deposit(accounts, "pedro", 5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void testNullDeposit(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = deposit(accounts, "mati", -5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void testWithdrawal(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "jaun", 5);
+        assertEquals(15, updatedAccounts.get("jaun"));
+    }
+    @Test
+    void testUnknownWithdrawal(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "pedro", 5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void testNullWithdrawal(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "jaun", -5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void TestNoBalanceWithdrawal(){
+        Map<String, Integer> accounts = Map.of("mati", 0, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "mati", 5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void TestTransfer(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "jaun", "mati", 5);
+        assertEquals(15, updatedAccounts.get("mati"));
+        assertEquals(15, updatedAccounts.get("jaun"));
+    }
+    @Test
+    void TestUnknownTransfer(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "jaun", "pedro", 5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void TestRecursiveTransfer(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "mati", 5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void Nulltransfer(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "jaun", "mati", -5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void TestNoBalance(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "jaun", 25);
+        assertEquals(accounts, updatedAccounts);
+    }
+
+
 }
